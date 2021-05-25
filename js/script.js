d9cc74d92d3afcf804a8d9b7a30bb6179b7f8108
Olek Myron <myrondell@gmail.com>
2021-05-25 16:20:27 +0300

    another variant with object

--- js/script.js
+++ js/script.js
@@ -1,15 +1,56 @@
+// // ---------- localStorage & forms
+
+// // import throttle from 'lodash.throttle';
+// const refs = {
+//   form: document.querySelector('.js-form'),
+//   textarea: document.querySelector('.js-feedback'),
+// };
+
+// const STORAGE_KEY = 'feedback-msg';
+
+// populateTextarea();
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.textarea.addEventListener('input', onTextareaInput);
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+
+//   e.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY); // При отправке очищаем localStorage
+// }
+
+// function onTextareaInput(e) {
+//   const value = e.target.value;
+//   localStorage.setItem(STORAGE_KEY, value);
+// }
+
+// // При перезагрузке вставляем savedMsg в textarea
+// function populateTextarea() {
+//   const savedMsg = localStorage.getItem(STORAGE_KEY);
+//   if (savedMsg) {
+//     refs.textarea.value = savedMsg;
+//   }
+// }
+
+// =====================================
+
 // ---------- localStorage & forms
+
+// import throttle from 'lodash.throttle';
 const refs = {
   form: document.querySelector('.js-form'),
   textarea: document.querySelector('.js-feedback'),
+  input: document.querySelector('.js-input'),
 };
 
 const STORAGE_KEY = 'feedback-msg';
+const formData = {};
 
 populateTextarea();
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', onTextareaInput);
+refs.form.addEventListener('input', onFormInput);
 
 function onFormSubmit(e) {
   e.preventDefault();
@@ -18,16 +59,23 @@ function onFormSubmit(e) {
   localStorage.removeItem(STORAGE_KEY); // При отправке очищаем localStorage
 }
 
-function onTextareaInput(e) {
-  const value = e.target.value;
-  localStorage.setItem(STORAGE_KEY, value);
-}
-
 // При перезагрузке вставляем savedMsg в textarea
 function populateTextarea() {
-  const savedMsg = localStorage.getItem(STORAGE_KEY);
-  if (savedMsg) {
-    refs.textarea.value = savedMsg;
+  // const savedMsg = localStorage.getItem(STORAGE_KEY);
+
+  const storage = JSON.parse(localStorage.getItem('formData'));
+
+  if (storage?.message) {
+    refs.textarea.value = storage.message;
+  }
+
+  if (storage?.userName) {
+    refs.input.value = storage.userName;
   }
 }
-// console.log('sent');
+
+function onFormInput(e) {
+  console.log(`key: ${e.target.name} , value: ${e.target.value}`);
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
